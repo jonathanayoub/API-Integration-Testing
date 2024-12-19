ca0c11a7e5f03ffeaf1d2347228617ec4fb204bb
jonathan.ayoub <jonathan.ayoub@kiewit.com>
2024-12-19 15:28:12 -0600

    implement AddressesController

---
+++ Controllers/AddressesController.cs
@@ -0,0 +1,19 @@
+﻿using ApiIntegrationTesting.Database;
+using ApiIntegrationTesting.Database.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiIntegrationTesting.Controllers;
+
+[ApiController]
+[Route("api/addresses")]
+public class AddressesController(AdventureWorksDbContext dbContext) : ControllerBase
+{
+    private readonly AdventureWorksDbContext _dbContext = dbContext ?? throw new ArgumentNullException(nameof(dbContext));
+    
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<AddressEntity>>> GetAddresses()
+    {
+        return await _dbContext.Addresses.ToListAsync();
+    }
+}
